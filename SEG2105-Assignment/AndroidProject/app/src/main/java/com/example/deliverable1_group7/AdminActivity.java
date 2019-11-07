735704cb61aafe8e9f75a88d2dbafb4b9d6a7c67
Polarheart <nicvittorini015@gmail.com>
2019-11-07 15:37:54 -0500

    Started work on Admin page

---
+++ SEG2105-Assignment/AndroidProject/app/src/main/java/com/example/deliverable1_group7/AdminActivity.java
@@ -0,0 +1,16 @@
+package com.example.deliverable1_group7;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+import android.os.Bundle;
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+        TextView textView = (TextView)findViewById(R.id.adTxt);
+    }
+}
