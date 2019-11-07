ce934829365271095c5b361c18f71370013c28b3
Henrychen666 <henryc00183001@gmail.com>
2019-11-07 16:37:52 -0500

    Create a new project which can connect to firebase Authentication

---
+++ SEGFinalProject2/app/src/main/java/com/example/segfinalproject/CreateAccount.java
@@ -0,0 +1,96 @@
+package com.example.segfinalproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CreateAccount extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText username;
+    private EditText useremail;
+    private EditText userpassword;
+    private Spinner usertype;
+    private Button register;
+    private Button login;
+
+    private FirebaseAuth firebaseauth;
+    private ProgressBar progress;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        firebaseauth = FirebaseAuth.getInstance();
+        progress = new ProgressBar(this);
+
+        username = (EditText) findViewById(R.id.username);
+        useremail = (EditText) findViewById(R.id.useremail);
+        userpassword = (EditText) findViewById(R.id.userpassword);
+        usertype = (Spinner) findViewById(R.id.usertype);
+        register = (Button) findViewById(R.id.createAccount);
+        login = (Button) findViewById(R.id.login);
+
+        register.setOnClickListener(this);
+        login.setOnClickListener(this);
+    }
+
+    public void AlreadyRegister(View view){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivityForResult(intent,0);
+    }
+
+    private void signupUser(){
+        String name = username.getText().toString().trim();
+        String email = useremail.getText().toString().trim();
+        String password = userpassword.getText().toString().trim();
+//        String type = usertype.getSelectedItem().toString().trim();
+
+        if (TextUtils.isEmpty(name)){
+            Toast.makeText(this, "Please fill all the Required information", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (password.length()<6){
+            Toast.makeText(this, "password should be at least 6 characters long", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progress.setVisibility(View.VISIBLE);
+
+        firebaseauth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progress.setVisibility(View.GONE);
+                        if (task.isSuccessful()){
+                            Toast.makeText(CreateAccount.this,"Register Successful",Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(CreateAccount.this,"Register Unsuccessful, Please Try again",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == register){
+            signupUser();
+        }
+    }
+}
---
+++ SEGFinalProject2/app/src/main/java/com/example/segfinalproject/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.segfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void OnEnter(View view) {
+//        Application Context and Activity
+//        Intent intent = new Intent(getApplicationContext(), WelcomeScreen.class);
+//        startActivityForResult(intent,0);
+    }
+
+    public void OnCreateAccount(View view) {
+        //Application Context and Activity
+        Intent intent = new Intent(getApplicationContext(), CreateAccount.class);
+        startActivityForResult(intent, 0);
+    }
+}
