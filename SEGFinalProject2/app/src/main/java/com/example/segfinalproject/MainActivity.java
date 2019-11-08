3dbb1f3315c461d871d1a6ed9f649e5a40ae0be5
Henrychen666 <henryc00183001@gmail.com>
2019-11-08 13:01:34 -0500

    login button works

--- SEGFinalProject2/app/src/main/java/com/example/segfinalproject/MainActivity.java
+++ SEGFinalProject2/app/src/main/java/com/example/segfinalproject/MainActivity.java
@@ -1,38 +1,83 @@
 package com.example.segfinalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity{
 
     private EditText useremail;
     private EditText userpassword;
+    private Button login;
+    private Button signup;
+
+    private FirebaseAuth firebaseauth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //useremail = ();
+
+        firebaseauth = FirebaseAuth.getInstance();
+        useremail = (EditText) findViewById(R.id.mainusermeail);
+        userpassword = (EditText) findViewById(R.id.userpasswordlogin);
+        login = (Button) findViewById(R.id.mainlogin);
+        signup = (Button) findViewById(R.id.createAccountBtn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginuser();
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OnCreateAccount(v);
+            }
+        });
+
+
     }
 
-    public void OnEnter(View view) {
-//        Application Context and Activity
-//        Intent intent = new Intent(getApplicationContext(), WelcomeScreen.class);
-//        startActivityForResult(intent,0);
-    }
+    private void loginuser(){
+        String email = useremail.getText().toString().trim();
+        String password = userpassword.getText().toString().trim();
 
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Please enter your Email address", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please enter your password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        firebaseauth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                    Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
     public void OnCreateAccount(View view) {
         //Application Context and Activity
         Intent intent = new Intent(getApplicationContext(), CreateAccount.class);
         startActivityForResult(intent, 0);
-    }
-
-    @Override
-    public void onClick(View v) {
-
     }
 }
