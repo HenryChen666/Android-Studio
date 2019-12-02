4001e0ddb7dc956ee5af198dbd78809364358694
Henrychen666 <henryc00183001@gmail.com>
2019-12-01 20:42:51 -0500

    clinicPage and book appointment popup page

---
+++ Deliverable 2/SEGFinalProject2/app/src/main/java/com/example/segfinalproject/bookappointment.java
@@ -0,0 +1,14 @@
+package com.example.segfinalproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class bookappointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookappointment);
+    }
+}
