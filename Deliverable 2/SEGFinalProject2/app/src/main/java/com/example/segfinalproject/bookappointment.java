8f9110576d617395213f7aa48beaf7a6aa41cae3
Henrychen666 <henryc00183001@gmail.com>
2019-12-05 23:24:50 -0500

    trying to get the hour to display

--- Deliverable 2/SEGFinalProject2/app/src/main/java/com/example/segfinalproject/bookappointment.java
+++ Deliverable 2/SEGFinalProject2/app/src/main/java/com/example/segfinalproject/bookappointment.java
@@ -1,10 +1,13 @@
 package com.example.segfinalproject;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class bookappointment extends AppCompatActivity {
+
+    ListView selecte_services;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
